107395d3afc3cf05856ea3bec1b95015171ff145
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-22 09:28:22 -0400

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -78,7 +78,7 @@ export default function SignUp() {
               <Label value='Your password' />
               <TextInput
                 type='password'
-                placeholder='Password'
+                placeholder='*********'
                 id='password'
                 onChange={handleChange}
               />
