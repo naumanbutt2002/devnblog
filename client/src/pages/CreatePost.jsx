b0cad0f8a585a85309235d5d45cebf80c0621b41
naumanbutt2002 <nauman100percent@gmail.com>
2024-09-21 18:54:30 +0500

    add show more functionality to the posts results of the dashboard

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -106,6 +106,8 @@ export default function CreatePost() {
             <option value='javascript'>JavaScript</option>
             <option value='reactjs'>React.js</option>
             <option value='nextjs'>Next.js</option>
+            <option value='flutter'>FLutter</option>
+            <option value='jetpack'>Jetpack Compose</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
